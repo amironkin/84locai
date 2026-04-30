329fea9fa699fb7e199a131c657231a6cbc747a3
Alexander Mironkin <amironkin@gmail.com>
2026-04-30 23:33:00 +0300

    Initial Commit

---
+++ 84locai/_4locaiApp.swift
@@ -0,0 +1,32 @@
+//
+//  _4locaiApp.swift
+//  84locai
+//
+//  Created by amironkin on 30.04.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct _4locaiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ 84locaiTests/_4locaiTests.swift
@@ -0,0 +1,16 @@
+//
+//  _4locaiTests.swift
+//  84locaiTests
+//
+//  Created by amironkin on 30.04.2026.
+//
+
+import Testing
+
+struct _4locaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
